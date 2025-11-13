43e9a485a11bd2d5224f05231802ab5f969ea041
abdulyazidi <imbxyz1@gmail.com>
2025-11-13 00:52:41 -0800

    learning go

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
 
 	gonanoid "github.com/matoous/go-nanoid/v2"
 
@@ -20,8 +21,9 @@ func main() {
 	if err != nil {
 		log.Fatal("Error connecting to db", err)
 	}
-
 	queries := sqlc.New(db)
+
+	app := &App{queries: queries}
 
 	url := "https://google.com"
 	shortenedURL, err := gonanoid.New(8)
@@ -48,5 +50,45 @@ func main() {
 
 	fmt.Printf("Shortened url: %v\nOriginal url: %v\n", shortenedURL, originalURL)
 
+	http.HandleFunc("/{$}", HomepageHandler)
+	http.HandleFunc("/", app.RedirectToOriginalURLHandler)
+	http.HandleFunc("/shorten", app.ShortenURLHandler)
+	http.ListenAndServe(":8090", nil)
 	fmt.Println("Exiting...")
+}
+
+func (app *App) RedirectToOriginalURLHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	shortURL := r.URL.Path[1:]
+
+	originalURL, err := app.queries.GetOriginalURL(ctx, shortURL)
+	if err != nil {
+		http.Error(w, "URL not found", http.StatusNotFound)
+		return
+	}
+
+	fmt.Println(r.URL.Path)
+	http.Redirect(w, r, originalURL, http.StatusMovedPermanently)
+}
+
+func (app *App) ShortenURLHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, "Failed to parse form: "+err.Error(), http.StatusInternalServerError)
+	}
+	shortURL, err := gonanoid.New(8)
+	if err != nil {
+		fmt.Println("Error")
+	}
+	fmt.Println(shortURL)
+	fmt.Fprintf(w, "Method: %s\n", r.Method)
+}
+
+func HomepageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL.Path)
+	fmt.Fprintf(w, "Homepage\n")
+}
+
+type App struct {
+	queries *sqlc.Queries
 }
